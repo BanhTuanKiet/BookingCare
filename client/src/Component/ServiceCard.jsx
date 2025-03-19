9a0c38fc0bb4ef8f28679025312d8113ebda13ed
wiki-1911 <datteo192004@gmail.com>
2025-03-19 16:36:28 +0700

    add images

--- client/src/Component/ServiceCard.jsx
+++ client/src/Component/ServiceCard.jsx
@@ -8,9 +8,10 @@ const ServiceCard = ({ service }) => {
 
   return (
     <Card 
-      className="shadow-sm border-0 p-3 service-card"
+      className="shadow-sm border-0 p-2 service-card"
       style={{ 
-        width: '18rem', 
+        width: '17rem', 
+        height: '',
         cursor: 'pointer', 
         transition: '0.3s', 
         border: '2px solid transparent'
@@ -18,35 +19,52 @@ const ServiceCard = ({ service }) => {
       onMouseEnter={(e) => {
         e.currentTarget.style.backgroundColor = 'darkturquoise';
         e.currentTarget.style.color = 'white';
+        e.currentTarget.querySelector('.service-icon').style.backgroundColor = 'white';
         e.currentTarget.style.border = '2px solid #0056b3';
       }}
       onMouseLeave={(e) => {
         e.currentTarget.style.backgroundColor = 'white';
         e.currentTarget.style.color = 'black';
+        e.currentTarget.querySelector('.service-icon').style.backgroundColor = '#0056b3';
         e.currentTarget.style.border = '2px solid transparent';
       }}
     >
-      <div className="d-flex align-items-start">
+      <div className="d-flex align-items-start m-1">
         <Card.Img 
           variant="top" 
-          src={serviceImage} 
-          alt={service.serviceName} 
-          className="img-fluid rounded service-icon" 
-          style={{ width: '15%', objectFit: 'cover' }}
+          src={serviceImage}
+          alt={service.serviceName}
+          className="img-fluid rounded service-icon p-1" 
+          style={{ 
+            width: '15%', 
+            objectFit: 'cover',
+            backgroundColor: '#0056b3',
+            transition: '0.3s' 
+          }}
         />
       </div>
-      <Card.Body className="text-center">
-        <Card.Title className="fw-bold text-start" style={{ fontSize: '1.2rem', marginBottom: '15px' }}>
+      <Card.Body className="text-left p-0 m-1 ">
+        <Card.Title 
+          className="fw-bold text-start"
+          style={{ fontSize: '1.2rem', marginBottom: '15px', padding: '0' }}
+        >
           {service.serviceName}
         </Card.Title>
-        <Card.Text className="text-start" style={{ marginBottom: '15px' }}>
+        <Card.Text 
+          className="text-start"
+          style={{ marginBottom: '15px', padding: '0' }}
+        >
           {service.description}
         </Card.Text>
-        <Card.Text className="fw-bold text-start" style={{ marginBottom: '15px', fontSize: '1.1rem' }}>
+        <Card.Text 
+          className="fw-bold text-start"
+          style={{ marginBottom: '15px', fontSize: '1.1rem', padding: '0' }}
+        >
           {service.price} VNĐ
         </Card.Text>
       </Card.Body>
     </Card>
   );
 }
+
 export default ServiceCard;
