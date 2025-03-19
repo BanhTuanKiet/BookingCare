b8d84145e85dc18d5242160277b0e03015e99f67
BanhTuanKiet <kiett5153@gmail.com>
2025-03-19 20:44:36 +0700

    fix ui

--- client/src/Component/Navigation.jsx
+++ client/src/Component/Navigation.jsx
@@ -69,7 +69,7 @@ const Navigation = () => {
   }
 
   return (
-    <Navbar expand="lg" style={{ backgroundColor: "#e3f1fc"}}>
+    <Navbar expand="lg" style={{ backgroundColor: "#f8f9fa"}}>
       <Container className="w-75 px-0">
         <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
