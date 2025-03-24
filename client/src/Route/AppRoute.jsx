c5e1c237b472f23e0d7659b8f9477eba491067f4
wiki-1911 <datteo192004@gmail.com>
2025-03-24 13:48:14 +0700

    decor ServiceCard and ServiceDetail

--- client/src/Route/AppRoute.jsx
+++ client/src/Route/AppRoute.jsx
@@ -10,6 +10,7 @@ import Appointment from '../Page/Appointment'
 import Specialty from '../Page/Specialty'
 import UploadDoctorImage from '../Page/UploadDoctorImage'
 import Login from '../Page/Login'
+import ServiceDetail from '../Page/ServiceDetail'
 
 function AppRoute() {
     return (
@@ -19,7 +20,7 @@ function AppRoute() {
             <Route path='/bác sĩ' element={<Index><Team /></Index>} />
             <Route path='/bác sĩ/:doctorName' element={<Index></Index>} />
             <Route path='/chuyên khoa/:specialty' element={<Index><Specialty /></Index>}></Route>
-            <Route path='/dịch vụ/:service' element={<Index></Index>}></Route>
+            <Route path='/dịch vụ/:serviceName' element={<Index><ServiceDetail/></Index>}></Route>
             <Route path='/tin tức' element={<Index><News /></Index>} />
             <Route path='/login' element={<Login /> } />
             <Route path='/đặt lịch khám' element={<Index><Appointment /></Index>} />
