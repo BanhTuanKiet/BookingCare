0c47e7c4d85c78e024cb48072c950667f2c5d9e0
BanhTuanKiet <kiett5153@gmail.com>
2025-05-19 13:11:58 +0700

    qwfescd

--- server/server/DTO/SpecialtyDTO.cs
+++ server/server/DTO/SpecialtyDTO.cs
@@ -2,6 +2,7 @@ namespace server.DTO
 {
     public class SpecialtyDTO
     {
+        public int SpecialtyId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string? SpecialtyImage { get; set; } // dạng base64
